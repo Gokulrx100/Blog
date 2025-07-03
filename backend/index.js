1dd641f4e9c97d3aad6c65d0498e7397d8ef78d3
Gokulrx100 <gokulkrishnakumar04@gmail.com>
2025-07-03 23:57:31 +0530

    minor improvements

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const blogRoutes = require("./router/blogRoutes");
+require("dotenv").config();
 
 app.use(express.json());
 
 async function ServerStart() {
   try {
-    let connect = await mongoose.connect(
-      "mongodb+srv://user123:tD4rUzrOZ0UwlGUI@cluster0.g0omesd.mongodb.net/blog-app?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    let connect = await mongoose.connect(process.env.MONGODB_URI);
     if (!connect) {
       console.log("Cannot connect to DB");
     } else {
